2351606bae5202bb19afc040980579e04962f470
Amann Malik <amannmalik@gmail.com>
2025-08-10 12:53:39 -0500

    refactor

--- src/main/java/com/amannmalik/mcp/core/Transport.java
+++ src/main/java/com/amannmalik/mcp/core/Transport.java
@@ -5,7 +5,7 @@ import jakarta.json.JsonObject;
 import java.io.IOException;
 
 /// - [Transports](specification/2025-06-18/basic/transports.mdx)
-public interface Transport extends AutoCloseable {
+interface Transport extends AutoCloseable {
     void send(JsonObject message) throws IOException;
 
     JsonObject receive() throws IOException;
