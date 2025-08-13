40f673c22cef59158890775a48a95160415f9879
Amann Malik <amannmalik@gmail.com>
2025-08-12 19:36:00 -0500

    refactor for public api

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,14 +1,9 @@
-module mcp.test {
+open module mcp.test {
     requires mcp.main;
     requires io.cucumber.java;
     requires io.cucumber.junit.platform.engine;
     requires org.junit.platform.suite.api;
-    exports com.amannmalik.mcp.test;
-    opens com.amannmalik.mcp.test to io.cucumber.junit.platform.engine;
-    requires io.cucumber.java;
     requires org.junit.jupiter.api;
-    requires io.cucumber.java;
     requires jakarta.json;
-    requires io.cucumber.java;
 
 }
